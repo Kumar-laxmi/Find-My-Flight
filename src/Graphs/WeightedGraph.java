e07df854372832e0b865e7da4cc0da2956a5531c
Kumar-laxmi <laxmikantk2002@gmail.com>
2022-05-03 15:56:42 +0530

    Graph implemented with 3 weights

--- src/Graphs/WeightedGraph.java
+++ src/Graphs/WeightedGraph.java
@@ -6,12 +6,16 @@ public class WeightedGraph {
 	 static class Edge {
 		 int source;
 		 int destination;
-		 int weight;
+		 int cost;
+		 int distance;
+		 int duration;
 		
-		 public Edge(int source, int destination, int weight) {
+		 public Edge(int source, int destination, int cost, int distance, int duration) {
 			 this.source = source;
 			 this.destination = destination;
-			 this.weight = weight;
+			 this.cost = cost;
+			 this.distance = distance;
+			 this.duration = duration;
 		 }
 	 }
 
@@ -28,8 +32,8 @@ public class WeightedGraph {
 			 }
 		 }
 		
-		 public void addEgde(int source, int destination, int weight) {
-			 Edge edge = new Edge(source, destination, weight);
+		 public void addEgde(int source, int destination, int cost, int distance, int duration) {
+			 Edge edge = new Edge(source, destination, cost, distance, duration);
 			 adjacencylist[source].addFirst(edge); //for directed graph
 		 }
 		
@@ -38,7 +42,8 @@ public class WeightedGraph {
 				 LinkedList<Edge> list = adjacencylist[i];
 				 for (int j = 0; j <list.size() ; j++) {
 					 System.out.println("vertex-" + i + " is connected to " +
-					 list.get(j).destination + " with weight " + list.get(j).weight);
+					 list.get(j).destination + " with cost " + list.get(j).cost 
+					 + " with distance " + list.get(j).distance + " with duration " + list.get(j).duration);
 				 }
 			 }
 		 }
@@ -46,15 +51,15 @@ public class WeightedGraph {
 	 public static void main(String[] args) {
 		 int vertices = 6;
 		 Graph graph = new Graph(vertices);
-		 graph.addEgde(0, 1, 4);
-		 graph.addEgde(0, 2, 3);
-		 graph.addEgde(1, 3, 2);
-		 graph.addEgde(1, 2, 5);
-		 graph.addEgde(2, 3, 7);
-		 graph.addEgde(3, 4, 2);
-		 graph.addEgde(4, 0, 4);
-		 graph.addEgde(4, 1, 4);
-		 graph.addEgde(4, 5, 6);
+		 graph.addEgde(0, 1, 4, 0, 1);
+		 graph.addEgde(0, 2, 3, 0, 1);
+		 graph.addEgde(1, 3, 2, 0, 1);
+		 graph.addEgde(1, 2, 5, 0, 1);
+		 graph.addEgde(2, 3, 7, 0, 1);
+		 graph.addEgde(3, 4, 2, 0, 1);
+		 graph.addEgde(4, 0, 4, 0, 1);
+		 graph.addEgde(4, 1, 4, 0, 1);
+		 graph.addEgde(4, 5, 6, 0, 1);
 		 graph.printGraph();
 	 }
 }

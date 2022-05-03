d1c185152bd3fdd1455e4c11eae005a37f6ec8bf
Sayan-sam <sayanbanerjee10012010@gmail.com>
2022-05-03 17:28:26 +0530

    Added combobox and draw coordinates

--- src/StartPage.java
+++ src/StartPage.java
@@ -1,6 +1,10 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.QuadCurve2D;
+import java.awt.geom.QuadCurve2D.Float;
 
 import javax.swing.*;
 
@@ -10,10 +14,19 @@ public class StartPage {
 	JPanel heading;
 	JPanel map;
 	JPanel input;
-	Point start, end;
 	JLabel X, Y;
 	Canvas draw;
-	JToggleButton tStart;
+	JComboBox<?> from, to;
+	
+	String[] toData = {"TO","DEL","BOM","BLR","HYD","CCU","MAA","AMD","GOI","PAT","COK","LKO","GAU","PNQ","JAI","SXR","BBI","IXB","VNS",
+			"IXC","IXR"};
+	String[] fromData = {"FROM","DEL","BOM","BLR","HYD","CCU","MAA","AMD","GOI","PAT","COK","LKO","GAU","PNQ","JAI","SXR","BBI","IXB","VNS",
+			"IXC","IXR"};
+	
+	String[] xco = {"0","211","109","211","238","458","277","107","130","398","182","288","540","156","182","162","413","460","336","206","392"};
+	String[] yco = {"0","190","390","524","427","318","530","301","466","249","586","222","227","405","223","63","355","223","259","143","297"};
+	
+	JButton connect;
 	
 	public StartPage()
 	{
@@ -27,11 +40,8 @@ public class StartPage {
 		heading = new JPanel();
 		map = new JPanel();
 		input = new JPanel();
-		tStart = new JToggleButton("Start");
-		X = new JLabel("X Coordinate is: ");
-		Y = new JLabel("Y Coordinate is: ");
-		start = new Point(109,389);
-		end = new Point(459,318);
+		X = new JLabel("x: ");
+		Y = new JLabel("y: ");
 		draw = new Canvas() {
 			private static final long serialVersionUID = 1L;
 
@@ -42,21 +52,33 @@ public class StartPage {
 				g.drawImage(image, 0, 0, this);
 			}
 		};
+		from = new JComboBox<Object>(fromData);
+		to = new JComboBox<Object>(toData);
+		connect = new JButton("FIND!");
 		
 		// Component Properties
-		tStart.setBounds(50, 500, 100, 50);
 		
 		X.setVisible(true);
-		X.setBounds(50,200,170,40);
+		X.setBounds(320,615,40,30);
+		
 		
 		Y.setVisible(true);
-		Y.setBounds(50,300,170,40);
+		Y.setBounds(360,615,40,30);
 		
 		draw.setBounds(0,0,660,680);
 		draw.setVisible(true);
 		
 		heading.setBounds(0,0,1080,100);
 		heading.setBackground(Color.yellow);
+		
+		from.setBounds(50,50,100,20);
+		from.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		
+		to.setBounds(200,50,100,20);
+		to.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		
+		connect.setBounds(125, 100, 80, 30);
+		connect.setFont(new Font("Times New Roman", Font.BOLD, 14));
 		
 		//Listeners and Actions
 		draw.addMouseListener(new MouseListener() {
@@ -68,9 +90,8 @@ public class StartPage {
 					int framex = e.getX();
 					int framey = e.getY();
 					System.out.println("x: "+framex+", y: "+framey);
-					X.setText("X Coordinate is: "+framex);
-					Y.setText("Y Coordinate is: "+framey);
-					drawLine(draw,start,end);
+					X.setText("x: "+(int)framex);
+					Y.setText("y: "+(int)framey);
 				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
@@ -90,14 +111,50 @@ public class StartPage {
 			
 		});
 		
+		to.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(to.getItemAt(to.getSelectedIndex()).equals(from.getItemAt(from.getSelectedIndex())))
+					to.setSelectedIndex(0);
+				
+			}
+			
+		});
+		
+		from.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(to.getItemAt(to.getSelectedIndex()).equals(from.getItemAt(from.getSelectedIndex())))
+					from.setSelectedIndex(0);
+			}
+			
+		});
+		
+		connect.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				resetCanvas(draw);
+				drawLine(draw, new Point(Integer.parseInt(xco[from.getSelectedIndex()]),Integer.parseInt(yco[from.getSelectedIndex()])), new Point(Integer.parseInt(xco[to.getSelectedIndex()]),Integer.parseInt(yco[to.getSelectedIndex()])));
+			}
+			
+		});
+		
 		//Panel Properties
 		map.setBounds(0,100,660,680);
 		map.add(draw);
 		
-		input.setBounds(660,100,420,620);
+		input.setBounds(660,100,420,680);
 		input.add(X);
 		input.add(Y);
-		input.add(tStart);
+		input.add(from);
+		input.add(to);
+		input.add(connect);
 		
 		map.setLayout(null);
 		input.setLayout(null);
@@ -113,7 +170,7 @@ public class StartPage {
 		startFrame.setSize(1080,780);
 	}
 	
-	public void drawLine(Canvas c, Point start, Point mouseEntered) {
+	public void drawLine(Canvas c, Point start, Point end) {
 		
 		Graphics g = c.getGraphics();
 		Graphics2D g2 = (Graphics2D) g;
@@ -123,8 +180,13 @@ public class StartPage {
 	                        BasicStroke.JOIN_MITER,
 	                        10.0f, dash1, 0.0f);
 	    g2.setStroke(dashed);
-	    g2.setBackground(Color.RED);
 		g2.drawLine(start.x, start.y, end.x, end.y);
+		
+		Float q = new QuadCurve2D.Float();
+		q.setCurve(start, new Point(330,340), end);
+		g2.setColor(Color.red);
+		g2.setStroke(new BasicStroke(2));
+		g2.draw(q);
 		
 	}
 	
@@ -137,9 +199,5 @@ public class StartPage {
 		g.drawImage(image, 0, 0, c);
 		
 	}
-	
-	
-	
-	
 
 }
